4452c17125ddb4c0e93d2647f1075c7e75477d8d
matsuo877 <64509243+matsuo877@users.noreply.github.com>
2020-07-23 17:47:43 +0900

    不具合修正

---
+++ main.php
@@ -0,0 +1,75 @@
+<?php
+namespace matsuo\TeamAPI;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\Player;
+
+class main extends PluginBase implements Listener {
+
+  private $teams = [];
+
+  public function joinTeam(string $teamName, Player $player) {
+    $playerName = $player->getName();
+    $members = [];
+    $members[] = $playerName;
+    $this->teams[$teamName] = $members;
+  }
+
+  public function leaveTeam(string $teamName, Player $player) {
+    $playerName = $player->getName();
+    if(($key = array_search($playerName, $this->teams[$teamName])) !== false) {
+      unset($this->teams[$teamName][$key]);
+    }
+  }
+
+  public function getTeam(Player $player) {
+    $name = $player->getName();
+    foreach($this->teams as $teamName -> $members) {
+      if(in_array($name, $members)) {
+        return ["team" => $teamName, "members" => $members];
+      }
+    }
+    return false;
+  }
+
+  public function sameTeam($playerA, $playerB): bool {
+    $nameA = $playerA->getName();
+    $nameB = $playerB->getName();
+    foreach($this->teams as $teamName => $members) {
+      if(in_array($nameA, $members) && in_array($nameB, $members)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public function onEnable() {
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+
+  public function onDamage(EntityDamageByEntityEvent $event) {
+    $atker = $event->getDamager();
+    $hiter = $event->getEntity();
+    if($atker instanceof Player && $hiter instanceof Player && $this->sameTeam($atker, $hiter)) {
+      $event->setCancelled();
+    }
+  }
+
+  public function onChat(PlayerChatEvent $event) {
+    $sender = $event->getPlayer();
+    $recipients = [];
+    $teamInfo = $this->getTeam($sender);
+    if($teamInfo !== false) {
+      foreach($event->getRecipients() as $player) {
+        if(in_array($player->getName(), $teamInfo["members"])) {
+          $recipients[] = $player;
+        }
+      }
+      $event->setRecipients($recipients);//チームに所属してなかったらここはスルーされて全員に送信されちゃうから、仕様にあわせて変更して
+    }
+  }
+}
+
